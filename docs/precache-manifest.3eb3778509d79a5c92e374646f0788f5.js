d80fc46dc28d4260f671bf72d57067a4983924a3
Eyal Shalev <eyalsh@gmail.com>
2019-12-17 10:55:10 +0200

    moved docs

---
+++ docs/precache-manifest.3eb3778509d79a5c92e374646f0788f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e05709594d1d95c8012bdd8150cb98f",
+    "url": "/use-fetch/index.html"
+  },
+  {
+    "revision": "0a76c850ac724402daf7",
+    "url": "/use-fetch/static/css/main.a063d359.chunk.css"
+  },
+  {
+    "revision": "21932111bb91bd357e42",
+    "url": "/use-fetch/static/js/2.0adfb735.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/use-fetch/static/js/2.0adfb735.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a76c850ac724402daf7",
+    "url": "/use-fetch/static/js/main.b8a04cbb.chunk.js"
+  },
+  {
+    "revision": "179bb1494973ddf8cd5a",
+    "url": "/use-fetch/static/js/runtime-main.815f3660.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/use-fetch/static/media/logo.5d5d9eef.svg"
+  }
+]);
